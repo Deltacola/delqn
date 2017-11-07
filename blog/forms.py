3cc8f6e12c1652270107264ecb72a15bcad4a918
Deltacola <deltacola90@gmail.com>
2017-11-07 22:20:47 +0900

    re

--- blog/forms.py
+++ blog/forms.py
@@ -1,13 +1,6 @@
 from django import forms
 
 from .choice import *
-from .models import Post
-
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ('title', 'text',)
 
 
 class DeliSearchForm(forms.Form):
--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.base, name='base'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^result.html$', views.result, name='result'),
     url(r'^search/$', views.SearchFormView.as_view(), name='search'),
 ]
