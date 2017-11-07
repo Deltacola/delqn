f6cd317d9fe8e2005da06fd2c3eb36c45a106bde
Deltacola <deltacola90@gmail.com>
2017-11-08 01:24:46 +0900

    171108

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class DeliveryData(models.Model):
     company_name = models.CharField(max_length=10)
-    weight = models.CharField(max_length=10)
-    total_length = models.CharField(max_length=10)
-    del_time = models.CharField(max_length=10)
+    weight = models.IntegerField(default=0)
+    total_length = models.IntegerField(default=0)
+    del_time = models.IntegerField(default=0)
     start_area = models.CharField(max_length=10)
     end_area = models.CharField(max_length=10)
-    price = models.CharField(max_length=10)
+    price = models.IntegerField(default=0)
--- import.py
+++ import.py
@@ -1,10 +1,10 @@
 import csv, sys, os
 
-project_dir = "C:/Users/Deltacola/Documents/deqn/delqn/delqn/"
+project_dir = "D:/deqn/delqn/"
 
 sys.path.append(project_dir)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'delqn.settings'
 
 import django
 
@@ -12,7 +12,7 @@ django.setup()
 
 from blog.models import DeliveryData
 
-data = csv.reader(open("C:/Users/Deltacola/Documents/deqn/delqn/jihachul.csv"), delimiter=',')
+data = csv.reader(open("D:/deqn/delqn/info.csv"), delimiter=',')
 
 for row in data:
     if row[0] != 'company_name':
@@ -20,7 +20,8 @@ for row in data:
         deli.company_name = row[0]
         deli.weight = row[1]
         deli.total_length = row[2]
-        deli.start_area = row[3]
-        deli.end_area = row[4]
-        deli.price = row[5]
+        deli.del_time = row[3]
+        deli.start_area = row[4]
+        deli.end_area = row[5]
+        deli.price = row[6]
         deli.save()
