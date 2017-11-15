5c701cf8befbb63fb49da4c7d2adc73c6abaeb17
Deltacola <deltacola90@gmail.com>
2017-11-15 12:39:43 +0900

    171115

--- blog/forms.py
+++ blog/forms.py
@@ -4,8 +4,8 @@ from .choice import *
 
 
 class DeliSearchForm(forms.Form):
-    start_point = forms.ChoiceField(choices = AREA_CHOICES, label="출발지역", initial='', widget=forms.Select(), required=True)
-    end_point = forms.ChoiceField(choices = AREA_CHOICES, label="도착지역", initial='', widget=forms.Select(), required=True)
-    weight_form = forms.ChoiceField(choices = WEIGHT_CHOICES, label="무게", initial='', widget=forms.Select(), required=True)
-    length_form = forms.ChoiceField(choices = LENGTH_CHOICES, label="총길이(가로+세로+높이)", initial='', widget=forms.Select(), required=True)
-    time_form = forms.ChoiceField(choices = TIME_CHOICES, label="최대 도착시간", initial='', widget=forms.Select(), required=False)
+    start_point = forms.ChoiceField(choices = AREA_CHOICES, label="From ", initial='', widget=forms.Select(), required=True)
+    end_point = forms.ChoiceField(choices = AREA_CHOICES, label="To ", initial='', widget=forms.Select(), required=True)
+    weight_form = forms.ChoiceField(choices = WEIGHT_CHOICES, label="무게 ", initial='', widget=forms.Select(), required=True)
+    length_form = forms.ChoiceField(choices = LENGTH_CHOICES, label="총길이(가로+세로+높이) ", initial='', widget=forms.Select(), required=True)
+    time_form = forms.ChoiceField(choices = TIME_CHOICES, label="최대 도착시간 ", initial='', widget=forms.Select(), required=False)
